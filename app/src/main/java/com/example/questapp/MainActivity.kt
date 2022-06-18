f33c1dfe7f97a4fa6b312af44fa66ec85736abf4
Fedor Anikeev <fedos3d@gmail.com>
2022-06-18 22:23:33 +0300

    Started working on RecyclerView for Quest

--- app/src/main/java/com/example/questapp/MainActivity.kt
+++ app/src/main/java/com/example/questapp/MainActivity.kt
@@ -9,24 +9,38 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.questapp.databinding.ActivityMainBinding
+import kotlin.math.log
+import kotlin.reflect.typeOf
 
 class MainActivity : AppCompatActivity() {
+    private val itemsList = ArrayList<String>()
+    private lateinit var customAdapter: CustomAdapter
+
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //kinda trying to create recycler view
+
+        customAdapter = CustomAdapter(itemsList)
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        println("${recyclerView::class.simpleName}")
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = customAdapter
+        prepareItems()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -39,6 +53,23 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
+    private fun prepareItems() {
+        itemsList.add("Item 1")
+        itemsList.add("Item 2")
+        itemsList.add("Item 3")
+        itemsList.add("Item 4")
+        itemsList.add("Item 5")
+        itemsList.add("Item 6")
+        itemsList.add("Item 7")
+        itemsList.add("Item 8")
+        itemsList.add("Item 9")
+        itemsList.add("Item 10")
+        itemsList.add("Item 11")
+        itemsList.add("Item 12")
+        itemsList.add("Item 13")
+        customAdapter.notifyDataSetChanged()
+        println("DEBUG")
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
@@ -48,11 +79,5 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
