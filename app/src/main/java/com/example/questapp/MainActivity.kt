1a59e82c575e2860efb72ed76f8b3a7c2f4dfae4
Fedor Anikeev <fedos3d@gmail.com>
2022-06-21 20:39:29 +0300

    Refined ScrollView, removed obsolete code, deleted unused methods

--- app/src/main/java/com/example/questapp/MainActivity.kt
+++ app/src/main/java/com/example/questapp/MainActivity.kt
@@ -2,23 +2,18 @@ package com.example.questapp
 
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.util.Log
 import android.view.View
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.questapp.data.Route
 import com.example.questapp.data.RoutePoint
 import com.example.questapp.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import com.yarolegovich.discretescrollview.DiscreteScrollView
-
-
-//import kotlinx.android.synthetic.main.activity_main.*
+import com.yarolegovich.discretescrollview.transform.Pivot
+import com.yarolegovich.discretescrollview.transform.ScaleTransformer
 
 class MainActivity : AppCompatActivity() {
     private lateinit var itemsList: ArrayList<Route>
@@ -30,45 +25,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         //IT SEEMS I GOTTA WRITE MULTITHREAD TO TRACK DISTANCE TO MARKER:
         //🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯🌯
 
-
-//      kinda trying to create recycler view, sorta works
         // Test Data Fill
         itemsList = ArrayList()
         prepareItems()
 
-
-//        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
-//        val customAdapter = CustomAdapter(this, itemsList)
-//
-//        val viewManager = LinearLayoutManager(this)
-//
-//        recyclerView.apply {
-//            layoutManager = viewManager
-//            adapter = customAdapter
-//        }
-//        customAdapter.notifyDataSetChanged()
-
         val customAdapter = CustomAdapter(this, itemsList)
         val scrollView = findViewById<DiscreteScrollView>(R.id.recyclerView)
+
+        // Adds smooth resizing
+        scrollView.setItemTransformer(
+            ScaleTransformer.Builder()
+                .setMaxScale(1.05f)
+                .setMinScale(0.8f)
+                .setPivotX(Pivot.X.CENTER) // CENTER is a default one
+                .setPivotY(Pivot.Y.BOTTOM) // CENTER is a default one
+                .build()
+        )
+
+        // Allows to scroll though more than one item
+        scrollView.setSlideOnFling(true);
+
+        // add
         scrollView.adapter = CustomAdapter(this, itemsList)
         customAdapter.notifyDataSetChanged()
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        setSupportActionBar(binding.toolbar)
 
         // Set action bar
         var actionBar: View? = findViewById(R.id.appBarLayout2)
         if (actionBar != null) {
             actionBar.isVisible = false
         }
-//
-        // Fab button
 
+        // Fab button
         var fab: View = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -76,11 +66,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
     private fun prepareItems() {
         var routedescr = "Маршрут номер 1(Тестовый)"
         var routepoints = ArrayList<RoutePoint>()
@@ -93,17 +78,6 @@ class MainActivity : AppCompatActivity() {
         itemsList.add(route.copy())
         itemsList.add(route.copy())
 
-        println("LIst: " + itemsList.size)
-        println("DEBUG")
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+        Log.d("","Route list size: " + itemsList.size)
     }
 }
